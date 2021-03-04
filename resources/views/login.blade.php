28282a507bce695b227b2835c25a6584604a5cd3
Mukhammadamin Abdullaev <mukhammadaminabdullaev01082002>
2021-03-04 10:37:38 +0500

    First Commits Made on Ecommerce Project

---
+++ resources/views/login.blade.php
@@ -0,0 +1,22 @@
+@extends('master')
+@section('content')
+<div class="container custom-login">
+	<div class="row">
+		<div class="col-sm-4 col-sm-offset-4">
+			<form action="login" method="post">
+				@csrf
+			  <div class="form-group">
+			    <label for="exampleInputEmail1">Email address</label>
+			    <input type="email" class="form-control" name="email" id="exampleInputEmail1" placeholder="Email">
+			  </div>
+			  <div class="form-group">
+			    <label for="exampleInputPassword1">Password</label>
+			    <input type="password" class="form-control" name="password" id="exampleInputPassword1" placeholder="Password">
+			  </div>
+
+			  <button type="submit" class="btn btn-default">Login</button>
+			</form>
+		</div>
+	</div>
+</div>
+@endsection
