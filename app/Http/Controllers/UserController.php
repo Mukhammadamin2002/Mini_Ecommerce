26f41b98002e40b04f9ad9b65359b724e1d25c95
Mukhammadamin Abdullaev <mukhammadaminabdullaev01082002>
2021-03-04 11:39:59 +0500

    Some Functionalities added on Login Page

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    function login(Request $req)
+    {
+    	$user = User::where(['email'=>$req->email])->first();
+    	if (!$user || !Hash::check($req->password,$user->password)) {
+    		return "Username or Password not matched";
+    	} else {
+    		$req->session()->put('user',$user);
+    		return redirect('/');
+    	}
+    }
+}
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,8 +4,8 @@
 	<div class="row">
 		<div class="col-sm-4 col-sm-offset-4">
 			<form action="login" method="post">
-				@csrf
 			  <div class="form-group">
+				@csrf
 			    <label for="exampleInputEmail1">Email address</label>
 			    <input type="email" class="form-control" name="email" id="exampleInputEmail1" placeholder="Email">
 			  </div>
